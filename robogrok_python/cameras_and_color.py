70fa05019764852311b873e09b2fecad0bf47b39
Sionnix <jyeo41@gmail.com>
2024-08-04 16:23:11 -0400

    sensors: set up openCV computer vision library
    Add instructions to set up openCV in the comments of the
    "cameras_and_color.py" file at the very top and confirm openCV works by
    running "import cv2".
    
    Angela Sodemann: Robotics 1 U2 (Sensors and Vision) S3 (Cameras and
    Color) P1 (Cameras and RGB)

---
+++ robogrok_python/cameras_and_color.py
@@ -0,0 +1,10 @@
+# Important: Download openCV from https://sourceforge.net/projects/opencvlibrary/files
+# Find openCVwin folder
+# Download 3.2.0 library version
+# Run the executable and extract it
+# Wait until the extraction is finished and then find the 'opencv' folder
+# Go to build > python > 2.7 > choose x64 version > copy cv2.pyd
+# Go to Python27 folder > lib > site-packages > paste cv2.pyd into this directory
+
+import numpy as np
+import cv2
