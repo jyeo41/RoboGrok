d81572020c68a585f6c8f48110abf646986d21b7
Sionnix <jyeo41@gmail.com>
2024-08-09 20:13:32 -0400

    P2 rack and pinion: modify code to use rack and pinion
    Hook up the DC motor with the rack and pinion on the last link of the
    2 degree of freedom revolute-revolute manipulator.
    
    Use two target_position variables so the rack and pinion can move
    between two points every 5 seconds. Notice how the control is still
    unstable and when the rack comes to a puse, the motor is still
    jittering violently.
    
    Angela Sodemann: Robotics 1 U3 (Motion Control) S2 (Open-Loop vs Feedback) P2 (Rack and Pinion)

--- robogrok_psoc_creator/robotics_1/motion_control.cydsn/main.c
+++ robogrok_psoc_creator/robotics_1/motion_control.cydsn/main.c
@@ -3,48 +3,31 @@
 
 int main(void)
 {
-    int counter1 = 0;
-    int counter2 = 0;
-    int counter_difference = 0;
+    int counter = 0;
     int time;
-    float counts_per_rev = 815.5;
-//    float speed = 0;
-    
-//    int compare = 0;
-    
+    int target_count1 = 1000;   // target positions for rack and pinion
+    int target_count2 = 2000;
+        
     quaddec_1_Start();
     lcd_char_1_Start();
     pwm_1_Start();
 
     for(;;)
     {
-        quaddec_1_SetCounter(0);
-        counter1 = quaddec_1_GetCounter();  // first count before the motor is on
-        
-        // the motor will turn in one direction because im setting one lead high, and the other lead low.
-        // only providing power to one lead and pulling the other to ground.
-        pwm_1_WriteCompare1(83);    // calculated compare value
-        pwm_1_WriteCompare2(0);
-        
-        counter2 = quaddec_1_GetCounter();  // Second count after the motor has been turned on
-        counter_difference = counter2 - counter1;
         time = 0;
         
-        // continue doing this control algorithm as long as our count difference is less than a quarter of a turn.
-        // this value is 204 counts. When we tested our motor we got 815.5 counts per revolution, divide that by 4.
-        // Give 5 seconds for the motor to reach the desired position
+        // continue doing this control algorithm as long as it hasn't reached 5 seconds
         while(time < 5000)
         {
-            counter2 = quaddec_1_GetCounter();  // Second count after the motor has been turned on
-            counter_difference = counter2 - counter1;
+            counter = quaddec_1_GetCounter();  // Second count after the motor has been turned on
             
             // if we are less than the target position, turn the motor forwards
-            if (counter_difference < (counts_per_rev / 4))
+            if (counter < target_count1)
             {
-                pwm_1_WriteCompare1(83);    // calculated compare value
+                pwm_1_WriteCompare1(83);    // higher compare value means faster rack and pinion speed but more instability
                 pwm_1_WriteCompare2(0);
             }
-            else if (counter_difference > (counts_per_rev / 4)) // if we are more than the target position, turn the motor backwards
+            else if (counter > target_count1) // if we are more than the target position, turn the motor backwards
             {
                 pwm_1_WriteCompare1(0);    // calculated compare value
                 pwm_1_WriteCompare2(83);                
@@ -55,15 +38,40 @@ int main(void)
                 pwm_1_WriteCompare2(0);                
             }
             CyDelay(1); // wait a millisecond and increment time variable
+            lcd_char_1_ClearDisplay();
+            lcd_char_1_Position(0, 0);
+            lcd_char_1_PrintNumber(counter);
             time += 1;
         }
         
-        CyDelay(43);    // wait delay should be the calculated 43ms
+        time = 0;
         
-        pwm_1_WriteCompare1(0);
-        pwm_1_WriteCompare2(0);
-        
-        CyDelay(5000);
-
+        // continue doing this control algorithm as long as it hasn't reached 5 seconds
+        while(time < 5000)
+        {
+            counter = quaddec_1_GetCounter();  // Second count after the motor has been turned on
+            
+            // if we are less than the target position, turn the motor forwards
+            if (counter < target_count2)
+            {
+                pwm_1_WriteCompare1(83);
+                pwm_1_WriteCompare2(0);
+            }
+            else if (counter > target_count2) // if we are more than the target position, turn the motor backwards
+            {
+                pwm_1_WriteCompare1(0); 
+                pwm_1_WriteCompare2(83);                
+            }
+            else    // if we are at the desired position, then turn the motor off
+            {
+                pwm_1_WriteCompare1(0);
+                pwm_1_WriteCompare2(0);                
+            }
+            CyDelay(1); // wait a millisecond and increment time variable
+            lcd_char_1_ClearDisplay();
+            lcd_char_1_Position(0, 0);
+            lcd_char_1_PrintNumber(counter);
+            time += 1;
+        }
     }
 }
