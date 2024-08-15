4bf8afc104c9f4d4796733da3ac3a62f0f7ac3a8
Sionnix <jyeo41@gmail.com>
2024-08-15 00:05:03 -0400

    P2 PSoC to computer: Modify code to send encoder values from PSoC to PC
    Use bit manipulation algorithm to send the counter values from 0 to 2000
    as a set of 2, 8 bit bytes to avoid the limitation of only being able to
    send up to the value 255 due to 8 bit restriction. Separate the two
    bytes in the PSoC code and then reassemble it and save those values in
    the Python code.
    
    From the python code, use data.txt to copy the values and plot a
    scatterplot in excel to see the position graph of the rack and pinion
    for two movements.
    
    Angela Sodemann: Robotics 1 U3 (Motion Control) S4 (UART) P2 (PSoC to Computer)

--- robogrok_psoc_creator/robotics_1/motion_control.cydsn/main.c
+++ robogrok_psoc_creator/robotics_1/motion_control.cydsn/main.c
@@ -11,8 +11,11 @@ int main(void)
     int speed = 0;  // the proportional speed used for linear control
     float kp = 3.0; // the control gain constant, higher values means more instability, speed, and accuracy
     uint8 receive;  // 8 bit byte to receive over uart transmission
+//    uint8 transmit;
+//    uint8 j;
+    uint8 byte1;
+    uint8 byte2;
     
-        
     quaddec_1_Start();
     lcd_char_1_Start();
     pwm_1_Start();
@@ -23,27 +26,38 @@ int main(void)
 
     for(;;)
     {
-        while(1)
+        // wait for a value to come over UART
+        // print the value to the LCD screen
+        receive = UART_1_GetChar();
+        
+        // anytime we ask the PSoC to receive a value over UART and no value has been received, the PSoC treats it as "0"
+        // keep polling until we receive a value
+        while (receive == 0)
         {
-            // wait for a value to come over UART
-            // print the value to the LCD screen
             receive = UART_1_GetChar();
-            
-            // anytime we ask the PSoC to receive a value over UART and no value has been received, the PSoC treats it as "0"
-            // keep polling until we receive a value
-            while (receive == 0)
-            {
-                receive = UART_1_GetChar();
-            }
-            lcd_char_1_ClearDisplay();
-            lcd_char_1_Position(0, 5);
-            lcd_char_1_PrintNumber(receive);
-            time += 10;
         }
+        
+//        while(1)
+//        {
+//
+//            lcd_char_1_ClearDisplay();
+//            lcd_char_1_Position(0, 5);
+//            lcd_char_1_PrintNumber(receive);
+//            
+//            transmit = 1;
+//            j = 1;
+//            while (transmit <= 10)
+//            {
+//                UART_1_PutChar(j);    // command to send value over UART
+//                CyDelay(200);
+//                transmit += 1;
+//                j += 10;
+//            }
+//        }
         time = 0;
         
-        // continue doing this control algorithm as long as it hasn't reached 5 seconds
-        while(time < 5000)
+        // continue doing this control algorithm as long as it hasn't reached 2 seconds
+        while(time < 2000)
         {
             counter = quaddec_1_GetCounter();  // Second count after the motor has been turned on
             error = target_count1 - counter;    // the error is the difference of where we want to be, and where we actually are
@@ -70,17 +84,25 @@ int main(void)
                 pwm_1_WriteCompare1(0);
                 pwm_1_WriteCompare2(speed);
             }
-            CyDelay(10); // wait a millisecond and increment the time value
             lcd_char_1_ClearDisplay();
             lcd_char_1_Position(0, 0);
             lcd_char_1_PrintNumber(counter);
+            
+            // algorithm to split the 16 bit assumed integer of counter to 2, 8 bit bytes so we can transfer it correctly over UART
+            // the algorithm keeps both byte1 and byte2 <= 255 which is what we want since that is the limit for 1 byte
+            byte1 = counter / 256;
+            byte2 = counter -(byte1 * 256);
+            UART_1_PutChar(byte1);    // we want to send the encoder count position of the rack and pinion over the UART
+            CyDelay(10);    // wait 10 milliseconds
+            UART_1_PutChar(byte2);
+            
             time += 10;
         }
         
         time = 0;
         
-        // continue doing this control algorithm as long as it hasn't reached 5 seconds
-        while(time < 5000)
+        // continue doing this control algorithm as long as it hasn't reached 2 seconds
+        while(time < 2000)
         {
             counter = quaddec_1_GetCounter();  // Second count after the motor has been turned on
             error = target_count2 - counter;    // the error is the difference of where we want to be, and where we actually are
@@ -107,10 +129,16 @@ int main(void)
                 pwm_1_WriteCompare1(0);
                 pwm_1_WriteCompare2(speed);
             }
-            CyDelay(10); // wait 10 milliseconds and increment time variable accordingly, this is to make the jittering more apparent
             lcd_char_1_ClearDisplay();
             lcd_char_1_Position(0, 0);
             lcd_char_1_PrintNumber(counter);
+            
+            byte1 = counter / 256;
+            byte2 = counter -(byte1 * 256);
+            UART_1_PutChar(byte1);    // we want to send the encoder count position of the rack and pinion over the UART
+            CyDelay(10);
+            UART_1_PutChar(byte2);
+            
             time += 10;
         }
     }
--- robogrok_python/uart_serial_communication.py
+++ robogrok_python/uart_serial_communication.py
@@ -11,9 +11,46 @@ i = bytearray([255]) # this is how we can send numbers between our PSoC and our 
 # serial.write(b'10') # 'b' means byte
 serial.write(i)
 
+
+# read values from the PSoC
+byte1 = []
+byte2 = []
+byte_assembled = []
+counter_values = []
+
+
+j = 0
+while (j < 200):    # run both loops 200 times to match the 2 second long move from the PSoC code
+    byte1 = bytearray([serial.read()])  # read in first 8 bits of the 16 bit counter
+    byte2 = bytearray([serial.read()])  # read in second 8 bits 
+    byte_assembled = byte2[0] + (byte1[0] * 256)    # assemble by doing the reverse algorithm, solving for counter
+    counter_values.append(byte_assembled)   # save the set of values into the array
+    j += 1
+
+j = 0
+while (j < 200):
+    byte1 = bytearray([serial.read()])
+    byte2 = bytearray([serial.read()])
+    byte_assembled = byte2[0] + (byte1[0] * 256)
+    counter_values.append(byte_assembled)
+    j += 1
+    
+
 serial.close()
 
+# the array 'b' stores the data sent from the PSoC to the PC so we are writing that data
+#   to a text file on our PC in order to paste it to excel and create a graph from it.
+# the objective is to run our position control code and send the value of the encoder count
+#   over the UART connection and save all those values and plot it on excel so we can precisely see
+#   how the motion happens.
+file = open("Data.txt", "w")
+i = 0
+while (i < 400):    # 400 times to account for both moves
+    file.write("%d\n" % counter_values[i])   # this is similar to printf() syntax from C
+    i += 1
 
+
+file.close()    # write to file won't be successful unless you close the file
 # Notes:
 # When sending serial.write(b'10') we see on the LCD screen as '48' why?
 # This is because we aren't sending the number '10', we are sending the characters '1' and '0'
