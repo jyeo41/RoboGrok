745cba09f9df41b779cfdd77bbed15157fbc4993
Sionnix <jyeo41@gmail.com>
2024-08-19 19:26:00 -0400

    comments: add comments to make code more understandable

--- pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/main.c
+++ pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/main.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-    pwm_1_Start();  // first start the pwm block
+    pwm_1_Start();  // first start the pwm block for the two revolute joint servos
     for(;;)
     {
-        manipulator_joint_1_angles(135.0, 180.0);
-        manipulator_joint_2_angles(-45.0, 0.0);
+        manipulator_joint_1_angles(135.0, 180.0);   // range from 0.0 to 180.0 degrees
+        manipulator_joint_2_angles(-45.0, 0.0);     // range from -90.0 to 90.0 degrees
     }
 }
