e61b3dff35c400d87d2be7883a8c811c828c6516
Sionnix <jyeo41@gmail.com>
2024-08-19 18:42:24 -0400

    feat(servos): implement servo movement using input angles
    Use a PWM block in PSoC Creator's top design to move the two revolute
    joints to desired positions using angles as the input. The angles are
    then calculated to the necessary compare values to write to the PWM
    block for each servo.
    
    The algebraic equation used is the 2 point form of the line equation
    which is the following:
    
    y - y1 = ((y2 - y1) / (x2 - x1)) * (x - x1)
    
    where 'y' is the compare value, and 'x' is the angle on the linear
    graph.
    
    The min/max compare values were tuned to be as accurate as possible at
    each 45.0 degree increment of angles. 0, 45, 90, 135, and 180 degrees.

--- pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/main.c
+++ pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/main.c
@@ -1,10 +1,12 @@
 #include "project.h"
+#include "scara_manipulator.h"
 
 int main(void)
 {
-
+    pwm_1_Start();  // first start the pwm block
     for(;;)
     {
-
+        manipulator_joint_1_angles(135.0, 180.0);
+        manipulator_joint_2_angles(-45.0, 0.0);
     }
 }
---
+++ pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/scara_manipulator.h
@@ -0,0 +1,9 @@
+#ifndef SCARA_MANIPULATOR_H
+#define SCARA_MANIPULATOR_H
+    
+float theta_1(float angle);
+float theta_2(float angle);
+void manipulator_joint_1_angles(float angle_1, float angle_2);
+void manipulator_joint_2_angles(float angle_1, float angle_2);
+    
+#endif  // SCARA_MANIPULATOR_H
