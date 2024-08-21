29e65161fd2663e857e7f5faa0c811eb4cfe6c51
Sionnix <jyeo41@gmail.com>
2024-08-21 02:59:39 -0400

    feat (xy positioning): refactor code to better implement encapsulation
    Improve code so the relevant kinematics equations are encapsulated into
    the "servos.c" file and "main.c" is not polluted with algorithmic code
    and lots of variables.
    
    The user can now just directly call the function
    "servos_position_set_xy()" and input a desired X and Y coordinate to
    move the manipulator, rather than modifying the X and Y variables in the
    main function.
    
    The links "a2" and "a4" were further tested and tuned, as well as
    checking the workspace of the manipulator through brute force method
    with a test function in the "test.c" file.

--- pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/main.c
+++ pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/main.c
@@ -1,30 +1,10 @@
 #include "servos.h"
 #include "electromagnet.h"
 #include "motor.h"
-#include "math.h"
+#include "test.h"
 
 int main(void)
-{   
-    float x = 4.0;    // desired x position as input
-    float y = 7.0;    // desired y position as input
-    
-    const float a2 = 5.2;   // link length constants, incresing this contsant decreases the x positioning of the end effector
-    const float a4 = 6.7;   // link length constant, increasing this constant decreases the y positioning of the end effector
-    float r1 = 0.0;
-    float phi_1 = 0.0;
-    float phi_2 = 0.0;
-    float phi_3 = 0.0;
-    float theta_1 = 0.0;
-    float theta_2 = 0.0;
-    
-    // inverse kinematics equations to calculate the values for the joint variables theta_1 and theta_2
-    r1 = sqrt((x * x) + (y * y));   // equation 1
-    phi_1 = acos(((a4 * a4) - (a2 * a2) - (r1 * r1)) / (-2.0 * a2 * r1));   // equation 2
-    phi_2 = atan(y / x);    // equation 3
-    theta_1 = phi_2 - phi_1;    // equation 4
-    phi_3 = acos(((r1 * r1) - (a2 * a2) - (a4 * a4)) / (-2.0 * a2 * a4));   // equation 5
-    theta_2 = 3.14159 - phi_3;    // equation 6
-    
+{       
     // Used to get the counter position of the dc motor.
     // c2 and c1 are digital INPUT signals.
     // Set to 32 bits and 4x resolution for maximum accuracy since the motor won't be running at max speed.
@@ -37,18 +17,17 @@ int main(void)
     
     for(;;)
     {
-        CyDelay(5000);  // give time to plug in the external PSU
-        // The initial position should be the rack at the top most position
-        servo_1_position_xy(theta_1, 1000);
-        servo_2_position_xy(theta_2, 3000);
-        rack_pinion_lower(5000);
-        rack_pinion_raise(3000);
+        CyDelay(3000);  // give time to plug in the external PSU
+//        test_manipulator_workspace();
         
-//        servo_1_position_xy(theta_1, 1000);
-//        servo_2_position_xy(theta_2, 2000);
-//        rack_pinion_lower(3000);
-//        rack_pinion_raise(3000);
-
+        servos_position_reset();
+        servos_position_set_xy(7.0, 9.0, 1000, 2000);
+        rack_pinion_lower(2000);
+        rack_pinion_raise(10);
+        servos_position_reset();
+        rack_pinion_lower(2000);
+        rack_pinion_raise(10);
+        
 //        electromagnet_on();
 //        CyDelay(2000);
 //        electromagnet_off();
--- pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/servos.h
+++ pick_and_place_manipulator/psoc_creator/robogrok1/pick_and_place_manipulator.cydsn/servos.h
@@ -1,5 +1,9 @@
 #ifndef SERVOS_H
 #define SERVOS_H
+
+// Make sure to include 'm' in the project > build settings > ARM GCC > assembler/compiler > general > additional include directories
+// Do the same for linker but you need to include 'm' for BOTH 'additional libraries' and 'additional library directories'
+#include <math.h>
     
 #include "project.h"
     
@@ -7,7 +11,9 @@ float theta_1(float angle);
 float theta_2(float angle);
 void servo_1_position_degrees(float angle, int delay);
 void servo_2_position_degrees(float angle, int delay);
+void servos_position_reset();
 void servo_1_position_xy(float theta, int delay);
 void servo_2_position_xy(float theta, int delay);
+void servos_position_set_xy(float x, float y, int delay_servo_1, int delay_servo_2);
     
 #endif  // SERVOS_H
