affb5c74a161158480881713112071990b392746
Sionnix <jyeo41@gmail.com>
2024-08-22 02:15:37 -0400

    feat (center of mass): find location of the object in units of pixels
    Use the 'center of mass' approach to get the object's location on the
    board grid as a coordinate of 2 pixels for both rows and columns.
    
    The user can now press the 'ESC' key on the initial 'background' image
    to grab a snapshot of a clear board, then put the nut on the board and
    it'll show up as a bright object in the 'difference' image frame. The
    python shell will show the coordinates of the nut in pixels.
    
    Also added a project notes section on the bottom to highlight critical
    errors and debugging.

--- pick_and_place_manipulator/python/pick_and_place_manipulator.py
+++ pick_and_place_manipulator/python/pick_and_place_manipulator.py
@@ -8,15 +8,16 @@
 
 import numpy as np
 import cv2
+np.seterr(divide='ignore', invalid='ignore')
 
 capture = cv2.VideoCapture(0)   # 0 is for the camera number connected to our PC
 
-# first capture the background without the target object inside of the picture
-# convert it to grayscale
-# convert it to black and white using the threshold algorithm
-# use the 'center of mass' approach to find the location of the object in units of pixels, for both row and cols
-# use pixel to centimeter to convert the location of the object in units of cm
-# use a homogenous transformation matrix to convert the position from the camera coordinate system to the base frame
+# First capture the background without the target object inside of the picture.
+# Convert it to grayscale.
+# Convert it to black and white using the threshold method.
+# Use the 'center of mass' approach to find the location of the object in units of pixels, for both rows and columns
+# Use pixel to centimeter to convert the location of the object in units of cm
+# Use a homogenous transformation matrix to convert the position from the camera coordinate system to the base frame
 
 # while loop to capture the first background frame
 while(1):
@@ -44,9 +45,53 @@ while(1):
     image_difference[image_difference > 255] = 255
     image_difference = np.uint8(image_difference)
     cv2.imshow('difference', image_difference)
+
+    image_black_white = image_difference
+    image_black_white[image_black_white <= 100] = 0
+    image_black_white[image_black_white > 100] = 1
+
+    # Algorithm to find the center of a bright object on a dark background of an image
+    column_sums = np.matrix(np.sum(image_black_white, 0))    # 0 parameter is for summing columns, 1 is for rows, 2 is for colors
+    column_numbers = np.matrix(np.arange(640))  # vector that holds the column numbers
+    column_multiply = np.multiply(column_sums, column_numbers) # ELEMENT wise multiplication with vectors/matrices
+    column_total = np.sum(column_multiply)  # get the total sum of the column values
+    matrix_total = np.sum(np.sum(image_black_white)) # total sum of the difference matrix, we have to np.sum() twice to achieve this
+    column_location = column_total / matrix_total   # calculated column center of the bright object in the difference image]
+
+##    print(column_location)
+
+    row_sums = np.matrix(np.sum(image_black_white, 1))  # sum along the rows axis, currently it has 480 rows and 1 column
+    row_sums = row_sums.transpose() # transpose the matrix so we have 1 row and 480 columns
+    row_numbers = np.matrix(np.arange(480))
+    row_multiply = np.multiply(row_sums, row_numbers)
+    row_total = np.sum(row_multiply)
+    matrix_total = np.sum(np.sum(image_black_white))
+    row_location = row_total / matrix_total
+
+##    print(row_location)
+
+    print(column_location, row_location)
     
     escape_key = cv2.waitKey(5)
     if(escape_key == 27):
         break
 
 cv2.destroyAllWindows()
+
+
+##### Project Notes #####
+#########################
+#
+# Problem: Weird values when trying to find the image_difference matrix
+# Solution: Realized I had to first convert the values to signed 16 bit integers so the calculations could handle potential negative
+#           pixel values. This is obviously invalid in an image matrix so then I had to use absolute value to rectify this problem.
+#           Afterwards, it was necessary to convert it back to unsigned 8 bit integers to keep the integrity of color matrix.
+# 
+# Problem: The image_black_white matrix was showing all 0's instead of 1's and 0's.
+# Solution: Small error but a critical one. The line [image_black_white <= 100] = 0 HAS to come first, otherwise it'll set all the
+#           pixels that got set to 1 immediately to 0 as well which is what we do not want.
+#
+#
+#
+#
+
